655aa10ac4b7dedb09c012913d0d50036c5f7bd4
quocvien0123 <viennq.23it@vku.udn.vn>
2024-08-30 11:00:46 +0700

    update

--- src/exercises/Demo.java
+++ src/exercises/Demo.java
@@ -3,5 +3,8 @@ package exercises;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        for (int i=1;i<10;i++){
+            System.out.println(i);
+        }
     }
 }
