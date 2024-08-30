3236457e7d7a8d0227ac1601a215714b7584901b
Quốc Viễn <164915955+quocvien0123@users.noreply.github.com>
2024-08-30 10:58:15 +0700

    Update Demo.java

--- src/exercises/Demo.java
+++ src/exercises/Demo.java
@@ -2,7 +2,7 @@ package exercises;
 
 public class Demo {
     public static void main(String[] args) {
-        for (i=1;i<=10;i++){
+        for (int i=1;i<=10;i++){
             System.out.println("Hello world");
         }
     }
